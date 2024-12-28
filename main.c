00c71bf5db7813b3e45702861d2319564e0d6164
ElliotZacharoff <Elliot.zacharoff@gmail.com>
2024-12-29 00:01:51 +0100

    Fixed: Search by files
    The problem was that  we were not reopening the file so it was still in write mode therefore not doing what we wanted it to.
    
    Also added so they get seperated by commas as well as ranking them in order of appearing

--- main.c
+++ main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
-//go get candy
-/*struct Ban
+
+/*struct Ban // Will use later for determining ranking and also selecting champion
 {
     int ranking;
     char[256] name;
@@ -11,7 +10,7 @@
 struct Champion
 {
     int ranking;
-    char[256]
+    char[256] name;
 }*/
 
 int main()
@@ -22,7 +21,6 @@ int main()
     settings = fopen("settings.txt", "r");
     if (settings == NULL)
     {
-        // Error
         settings = fopen("settings.txt", "w");
         /* Code inbetween
         Bans: [up to 4 chapmions]
@@ -31,32 +29,56 @@ int main()
 
         fprintf(settings, "Bans: \nChampions: ");
         fclose(settings);
-    }
 
+        settings = fopen("settings.txt", "r"); // REOPENNNNN (sat with this for an hour before giving up and asking the gpt)
+        if (settings == NULL)
+        {
+            printf("Unable to reopen");
+            return 1;
+        }
+    }
     char buffer[256];
 
-    char b[6] = "Bans: ";
-    char c[12] = "Champions: ";
+    char bans[256] = "";
+    char champions[256] = "";
 
-    char bans[256];
-    char champions[256];
-
-    while (fscanf(settings, "%[^\n]\n", buffer) != EOF)
+    while (fscanf(settings, "%[^\n]\n", buffer) != EOF) // still no clue tf the "%[^\n]\n" does but we ball
     {
         buffer[strcspn(buffer, "\n")] = '\0';
 
-        if (strncmp(buffer, b, strlen(b)) == 0)
+        if (strncmp(buffer, "Bans: ", 6) == 0) // 3rd param meaning everything after index 6
         {
-            strcpy(bans, buffer);
+            strcpy(bans, buffer + 6);
         }
-        else if (strncmp(buffer, c, strlen(c)) == 0)
+        else if (strncmp(buffer, "Champions: ", 11) == 0) // After index 11
         {
-            strcpy(champions, buffer);
+            strcpy(champions, buffer + 11);
         }
     }
-    printf("B: %s\n", bans);
-    printf("C: %s\n", champions);
-
     fclose(settings);
+
+    int rankBans = 1;
+    int rankChampions = 1;
+
+    // Split bans by ','
+    printf("Bans:\n");
+    char *token = strtok(bans, ",");
+    while (token != NULL)
+    {
+        printf("- %s [ranking: %d]\n", token, rankBans);
+        token = strtok(NULL, ",");
+        rankBans += 1;
+    }
+
+    // Split champions by ','
+    printf("Champions:\n");
+    token = strtok(champions, ",");
+    while (token != NULL)
+    {
+        printf("- %s [ranking: %d]\n", token, rankChampions);
+        token = strtok(NULL, ",");
+        rankChampions += 1;
+    }
+
     return 0;
 }
